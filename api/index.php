0f1445597e0db8725ce7742a5c8e8075c411875b
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-09 17:14:52 +0100

    Add first version genre filter to fix

--- api/index.php
+++ api/index.php
@@ -6,7 +6,17 @@ $source_url = __DIR__ . '../../data.json';
 // Read file content
 $json_data = file_get_contents($source_url);
 
+
 $disks = json_decode($json_data, true);
+
+//rescue select value
+$genre = $_GET['genre'] ?? '';
+
+if (!empty($genre)) {
+    $filter_disk = array_filter($disks, fn ($disk) => strtolower($disk['genre']) === $genre);
+    $disks = $filter_disk;
+}
+
 
 //Say to server that I use json lenguage
 header('Content-Type: application/json');
