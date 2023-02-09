94ab555a977800ea2245962522c7bffb026a0e43
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-09 15:23:27 +0100

    change js file

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
+
+
 // CHECK JS LINK
-console.log('JS OK');
+const apiUri = 'http://localhost:8080/php-dischi-json/index.php';
+
+//? CREAZIONE APP DI VUE
+
+const app = Vue.createApp({
+    data() {
+        return {
+            disks: [],
+        }
+    },
+    mounted() {
+        axios.get(apiUri).then(res => {
+            this.disks = res.data;
+        })
+    }
+});
+console.log(this.disks)
+app.mount('#root');
